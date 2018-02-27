d37ab1cf06bef21295c6c33a8bc571f6a1c04932
Victor Soria <victorspgl@gmail.com>
2018-02-27 10:47:36 +0100

    Arreglado la puta mierda esa

--- __main__.py
+++ __main__.py
@@ -1,4 +1,15 @@
-# AEAEAEAEAE
+# Programa principal
+# Victor Soria Pardos
+# 7 Febrero 2018
 
-if __name__ == "__main__":
-    print("Hello world")
+
+while True:
+    nombre_fichero = input("Introduce fichero de entrada: ")
+
+    configuracion = lectura_problema(nombre_fichero)
+
+
+
+    
+
+    almacenar_solucion(solucion)
