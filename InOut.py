bd71b1584f9a233919fa25bf5c1a46f73c6e1295
carlosmn1997 <carlosmn1997@gmail.com>
2018-02-27 10:52:08 +0100

    done la lectura y el objeto

--- InOut.py
+++ InOut.py
@@ -25,19 +25,33 @@ def lectura_problema(nombreFichero):
 
     contadorLineas = 2
     x, y = 0, 0 # x son los numEndpoints, y son los caches
-    matrixLatency = np.zeros((numEndpoints, (numCaches+1)), dtype=np.uint8)
+    matrixLatency = np.zeros((numEndpoints, (numCaches+1)))
     salir = False
     while (salir == False):
+
         linea = lineas[contadorLineas]
-        if (len(linea.split()) != 2):
+        partida = linea.split()
+        latencyDatacenter = map(int, partida)
+        if (len(latencyDatacenter) != 2):
             salir = True
         else:
-            matrixLatency[x][y] = 
+            contadorLineas = contadorLineas + 1
+            matrixLatency[x][y] = latencyDatacenter[0]
+            for i in range(1, latencyDatacenter[1]+1):
+                linea = map(int, lineas[contadorLineas].split())
+                contadorLineas = contadorLineas + 1
+                matrixLatency[x][linea[0]+1] = linea[1]
+            x = x + 1
 
-#     config.setParametrosIniciales(filas, columnas, min, max)
-#     for i in range(0,config.getDimensionx()):
-#         for j in range(0,config.getDimensiony()):
-#             config.setAtributo(i, j, lineas[i+1][j])
-# return config
-    print numVideos, numEndpoints, numRequests, numCaches, sizeCache, sizeVideos
+    matrixRequests = np.zeros((numEndpoints, numVideos))
+    for i in range(contadorLineas, len(lineas)):
+        linea = map(int, lineas[contadorLineas].split())
+        contadorLineas = contadorLineas + 1
+        # matrixRequests[endPoint][video]
+        matrixRequests[linea[1]][linea[0]] = linea[2]
+
+    config = Configuracion()
+    config.setParametrosIniciales(numVideos, numEndpoints, numRequests, numCaches, sizeCache, sizeVideos, matrixLatency, matrixRequests)
+    return config
+
 lectura_problema('me_at_the_zoo.in')
